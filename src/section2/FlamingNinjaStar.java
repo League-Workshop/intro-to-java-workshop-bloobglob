af1e22248d846cd09a51bf03fc2733fd1807e026
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-04-06 15:24:52 -0700

    s

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -12,43 +12,42 @@ public class FlamingNinjaStar {
 		int flameSize = 200;		//the length of the flaming arms
 		
 		// 1. Make a new robot, and set it's pen down.
-Robot robot = new Robot();
-robot.setPenColor(Color.black);
-robot.penDown();
-robot.setPenWidth(2);
-robot.hide();
+		Robot poopy = new Robot ();
+		poopy.penDown();
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
+		poopy.setX(450);
+		poopy.setY(450);
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
-
 		// 12. Set the robot speed to 100
-robot.setSpeed(100);
+		poopy.setSpeed(100);
 		// 13. Use a for loop to repeat all of the code below 25 times
-for(int i = 0; i < 25; i++) {
+for (int i = 0; i < 25; i++) {
+	
+
 			// 2. Turn the robot 1/8 of a circle
-		robot.turn(360/8);
+		poopy.turn(45);
+		poopy.setPenColor(Color.orange);
 			// 3. Move the robot 64 pixels
-robot.move(64);
+		poopy.move(64);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			robot.turn(-40);
+			poopy.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			robot.setPenColor(Color.orange);
-			robot.move(flameSize);
+			poopy.move(flameSize);
+			
 			// 6. Turn the robot 170 degrees
-			robot.turn(170);
+			poopy.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			robot.move(flameSize);
-			robot.setPenColor(Color.black);
+			poopy.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			robot.turn(64);
+			poopy.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			robot.move(baseSize);
+			poopy.setPenColor(Color.black);
+			poopy.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			
 			// 11. Color your ninja star like Figure 2.
-		
+}
 	}}
 
-}
 
 
--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,36 +6,36 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-Robot guy = new Robot();
+Robot fat = new Robot ();
 	void go() {
 		// 4. Make the robot move as fast as possible
-		guy.setSpeed(1000);
-		guy.penDown();
-		guy.hide();
+		fat.setSpeed(100);
 		// 5. Set the pen width to 5
-		guy.setPenWidth(5);
+		fat.penDown();
+		fat.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-for(int i = 0; i < 4; i++) {
-guy.setRandomPenColor();
-guy.sparkle();
-guy.turn(-90);
-drawSquare();}
-}
-	// 7. Set the pen color to random
+for (int i = 0; i < 4; i++) {
 	
-			// 1. Call the drawSquare() method
-			// 8. Turn the robot 90 degrees to the right
 
+	// 7. Set the pen color to random
+	fat.setRandomPenColor();
+			// 1. Call the drawSquare() method
+		drawSquare();
+			// 8. Turn the robot 90 degrees to the right
+		fat.turn(90);
+	}}
 
 	
 	void drawSquare() {
 		//JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		for(int i = 0; i < 4; i++) {
+		for (int i = 0; i < 4; i++) {
+			
+	
 		//move
-		guy.move(400);
+		fat.move(50);
 		//turn
-		guy.turn(-90);
+		fat.turn(90);
 	}}
 
 	public static void main(String[] args) {
