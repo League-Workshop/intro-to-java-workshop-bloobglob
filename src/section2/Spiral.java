5df17b8b280118cd4c36b4f43871f7b869968960
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-04-06 19:55:19 -0700

    a

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,21 +6,12 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		Robot guy = new Robot();
-		guy.hide();
-		// 5. Set your robot's pen down 
-		guy.penDown();
+		// 5. Set your robot's pen down
 		// 3. Set the robot to go at max speed (100)
-		guy.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		for(int i = 0; i < 75; i++) {
 			// 7. Change the pen color to random
-		guy.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			guy.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		guy.turn(360/7);
 			// 8. Set the pen width to i
-			guy.setPenWidth(i);
 	}
 }}
